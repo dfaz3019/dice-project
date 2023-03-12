665a6520b122874cea972c7e11d6f2ffc64f11dd
Dana Fazeli <danafazeli@hotmail.co.uk>
2023-03-12 21:05:54 +1100

    Added reset button + select option

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const button = document.querySelector('#button');
-const diceDiv = document.querySelector('#dice-display');
+var rollDiceButton = document.querySelector('#button');
+var resetButton = document.querySelector('#reset')
+var diceImageDiv = document.querySelector('#dice-container');
+var diceDropDown = document.querySelector('#dice-quantity');
 var randomNumber = null;
+var diceQuantity = document.querySelector('#dice-quantity').value
 
-button.addEventListener('click', () => {
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
-    console.log(randomNumber);
+diceDropDown.addEventListener('input', () => {
+    diceQuantity = document.querySelector('#dice-quantity').value
+})
 
-    diceDiv.setAttribute('src', `/images/dice/dice${randomNumber}.svg`)
+function initialiseDiceQuantity () {
+
+    for (let i = 0; i < diceQuantity; i++) {
+        var randomNumber = Math.floor(Math.random() * 6) + 1;
+        var imgContainer = document.createElement('img');
+        diceImageDiv.append(imgContainer);
+        imgContainer.id = `dice-display${i}`;
+        imgContainer.classList.add('die-image')
+        imgContainer.setAttribute('src', `/images/dice/dice${randomNumber}.svg`)
+    }
+}
+
+rollDiceButton.addEventListener('click', () => {
+    initialiseDiceQuantity()
+})
+
+resetButton.addEventListener('click', () => {
+        for (let i = 0; i < diceQuantity; i++) {
+        diceImageDiv.removeChild(diceImageDiv.firstChild)
+    }
 })
 
